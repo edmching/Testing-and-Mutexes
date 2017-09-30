bd91a35e8fd8aa7d2820457290c68fa2a8ac6a4f
edmching <edmching@hotmail.com>
2017-09-30 00:49:05 -0700

    working wordcount

--- src/word_count.cpp
+++ src/word_count.cpp
@@ -4,8 +4,6 @@
 int word_count(const std::string& line, int start_idx) {
 
   // YOUR IMPLEMENTATION HERE
-
-
   int nwords = 0;
   for (int i=start_idx; i<line.length(); ++i) {
     if (line[i] == ' ' && line[0] != ' ') {
